f88f9e3d2c9fa09b4529b82077a273ca58256cb2
claimit_script <kalpesh.tps@gmail.com>
2020-01-22 19:11:20 +0530

    no message

---
+++ Test Suites/mobile/DEMO.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DEMO</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e1817e93-8040-4ea3-b849-ed229ad989c9</testSuiteGuid>
+   <testCaseLink>
+      <guid>98b4caec-fe94-4eae-ad54-e7b59397b591</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mobile/stage_1/Enter_Password/Entret_password_successful/Entret_password_successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5317fec2-1b2a-426f-a8e9-e95ba39bc0ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mobile/stage_1/end_user_license_agreement/end_user_license_agreement</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
